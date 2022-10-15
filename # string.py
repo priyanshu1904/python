ecff2fe770e97fc507af1e5b3401030ac3fc08f5
priyanshu1904 <115339817+priyanshu1904@users.noreply.github.com>
2022-10-15 08:29:30 +0530

    Add files via upload

---
+++ # string.py
@@ -0,0 +1,58 @@
+# string
+
+# x='my name is priyanshu'
+# print(x[1:9])
+# print(x[4:])
+
+# print(x[-4:-3])
+
+# print(x.upper())
+
+# print(x.split())
+# y=x.split()
+# print(y[2])
+
+# z='its sunday today'
+# print(x+' '+z)
+
+
+# Boolean
+
+# print(bool('weather is pleasent today'))
+# print(bool(9>99))
+# print(bool(2<19))
+ 
+# print(bool(0))
+# print(bool([]))
+# print(bool({}))
+
+
+# List
+
+a=[10,20,50,'pen','phone']
+#print(type(a))
+#a=[]
+#print(a)
+#print(len(a))
+#print(a[:])
+#print(a[-5:-3])
+#print(a[2:4])
+
+#a[2:4]=[30,40]
+#print(a)
+#a[2:3]=[100,200]
+#print(a)
+
+#a.insert(2,8)
+#print(a)
+#a.append(80)
+#print(a)
+
+#b=[100,200,300]
+#a.extend(b)
+#print(a)
+
+#a.remove('pen')
+#print(a)
+#a.pop(4)
+#print(a)
